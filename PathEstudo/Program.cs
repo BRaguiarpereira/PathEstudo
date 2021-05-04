8459e6570c3b7edaf31cd5624084f3d3469dafad
Brian aguiar <briantbini@outlook.com>
2021-05-04 11:03:36 -0300

    Função path importante para pegar caminho de pastas

---
+++ PathEstudo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace PathEstudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\myfolder\open01.txt";
+
+            Console.WriteLine("DirectorySeparatorChar : " + Path.DirectorySeparatorChar);
+            // deixa a porção(caminho) do arquivo de lado 
+            Console.WriteLine("GetDirectoryName : " + Path.GetDirectoryName(path));
+
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+
+            Console.WriteLine("GetFileName :" + Path.GetFileName(path));
+
+            Console.WriteLine("GetFileNameWithoutExtension : " + Path.GetFileNameWithoutExtension(path));
+
+            Console.WriteLine("GetExtension :" + Path.GetExtension(path));
+
+            Console.WriteLine("GetFullPath : " + Path.GetFullPath(path));
+        }
+    }
+}
